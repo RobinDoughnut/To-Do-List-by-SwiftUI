7f441686c347b006b63e7fc5c5f9feb265163603
Taasin Alvi <77426735+RobinDoughnut@users.noreply.github.com>
2023-01-09 02:10:17 +0600

    Initial Commit

---
+++ ToDoListTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoListTutorial
+//
+//  Created by Taasin Bin Hossain Alvi on 1/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoListTutorial/ToDoListTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTutorialApp.swift
+//  ToDoListTutorial
+//
+//  Created by Taasin Bin Hossain Alvi on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
